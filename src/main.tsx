79c3fca62bb93f6153f228dfdc94369861746028
mahmoudbadawi-lingobite <mahmoud.badawi@alkifah.edu.sa>
2026-06-29 10:07:07 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
-import '@/lib/seedFirestore' // Exposes window.seedLingoBite() for one-time DB seeding
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
