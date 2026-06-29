123e45247d0f53b4dc3acb59eeb6e3d309da2342
mahmoudbadawi-lingobite <mahmoud.badawi@alkifah.edu.sa>
2026-06-29 10:02:39 +0300

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.tsx'
+import '@/lib/seedFirestore' // Exposes window.seedLingoBite() for one-time DB seeding
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
