8d59706d075428b1b950d0b9a3abddbd80a68be3
mahmoudbadawi-lingobite <official.mahmoudbadawi@gmail.com>
2026-07-11 18:38:35 +0300

    Add files via upload

--- src/lib/avatars.ts
+++ src/lib/avatars.ts
@@ -1,9 +1,8 @@
 // ============================================================
 // LingoBite - Avatar Options
 // ------------------------------------------------------------
-// These are the 30 avatar images uploaded to Cloudinary
-// (cloud name: djsoffa6h). If you add/replace avatars later,
-// update this list with the new URLs from your Media Library.
+// 30 avatar images. Update this list if avatars are replaced
+// again later.
 // ============================================================
 
 export interface AvatarOption {
@@ -12,35 +11,34 @@ export interface AvatarOption {
 }
 
 export const AVATAR_OPTIONS: AvatarOption[] = [
-  { id: 'avatar_1', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461319/1_bn2ibn.png' },
-  { id: 'avatar_2', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461317/2_wbm3dm.png' },
-  { id: 'avatar_3', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461235/3_lziygm.png' },
-  { id: 'avatar_4', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461109/4_opt2re.png' },
-  { id: 'avatar_5', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461108/5_ubhllj.png' },
-  { id: 'avatar_6', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461121/6_vkz3q9.png' },
-  { id: 'avatar_7', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461238/7_acin9x.png' },
-  { id: 'avatar_8', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461246/8_ir1blx.png' },
-  { id: 'avatar_9', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461119/9_rnamv4.png' },
-  { id: 'avatar_10', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461130/10_isxp1y.png' },
-  { id: 'avatar_11', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461235/11_bq3qqz.png' },
-  { id: 'avatar_12', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461132/12_smbckp.png' },
-  { id: 'avatar_13', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461251/13_h6nihd.png' },
-  { id: 'avatar_14', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461139/14_huxie5.png' },
-  { id: 'avatar_15', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461145/15_qcco7k.png' },
-  { id: 'avatar_16', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461150/16_hhbe3e.png' },
-  { id: 'avatar_17', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461157/17_jconql.png' },
-  { id: 'avatar_18', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461165/18_xnec7v.png' },
-  { id: 'avatar_19', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461173/19_jentiu.png' },
-  { id: 'avatar_20', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461183/20_yej3uj.png' },
-  { id: 'avatar_21', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461193/21_vgngxh.png' },
-  { id: 'avatar_22', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461203/22_bsrsaj.png' },
-  { id: 'avatar_23', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461210/23_oj5p5a.png' },
-  { id: 'avatar_24', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461216/24_ilwbba.png' },
-  { id: 'avatar_25', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461222/25_brujad.png' },
-  { id: 'avatar_26', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461240/26_j8tpz4.png' },
-  { id: 'avatar_27', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461271/27_ny2gwf.png' },
-  { id: 'avatar_28', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461259/28_z2ta6p.png' },
-  { id: 'avatar_29', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461261/29_frfteq.png' },
-  { id: 'avatar_30', url: 'https://res.cloudinary.com/djsoffa6h/image/upload/v1783461250/30_zmm7qp.png' },
+  { id: 'avatar_1', url: 'https://files.catbox.moe/hq0w0a.png' },
+  { id: 'avatar_2', url: 'https://files.catbox.moe/dvlafy.png' },
+  { id: 'avatar_3', url: 'https://files.catbox.moe/yl3rh8.png' },
+  { id: 'avatar_4', url: 'https://files.catbox.moe/j0n628.png' },
+  { id: 'avatar_5', url: 'https://files.catbox.moe/03u1b9.png' },
+  { id: 'avatar_6', url: 'https://files.catbox.moe/i8n1l6.png' },
+  { id: 'avatar_7', url: 'https://files.catbox.moe/2u6xt6.png' },
+  { id: 'avatar_8', url: 'https://files.catbox.moe/tfl021.png' },
+  { id: 'avatar_9', url: 'https://files.catbox.moe/d2x13p.png' },
+  { id: 'avatar_10', url: 'https://files.catbox.moe/q6l6fx.png' },
+  { id: 'avatar_11', url: 'https://files.catbox.moe/qdwygd.png' },
+  { id: 'avatar_12', url: 'https://files.catbox.moe/ywuqr0.png' },
+  { id: 'avatar_13', url: 'https://files.catbox.moe/cip4qt.png' },
+  { id: 'avatar_14', url: 'https://files.catbox.moe/5t4a74.png' },
+  { id: 'avatar_15', url: 'https://files.catbox.moe/3vj49e.png' },
+  { id: 'avatar_16', url: 'https://files.catbox.moe/pg787f.png' },
+  { id: 'avatar_17', url: 'https://files.catbox.moe/1tzx37.png' },
+  { id: 'avatar_18', url: 'https://files.catbox.moe/2xpu4m.png' },
+  { id: 'avatar_19', url: 'https://files.catbox.moe/dwyg88.png' },
+  { id: 'avatar_20', url: 'https://files.catbox.moe/2d31v7.png' },
+  { id: 'avatar_21', url: 'https://files.catbox.moe/q770ll.png' },
+  { id: 'avatar_22', url: 'https://files.catbox.moe/lcqcl4.png' },
+  { id: 'avatar_23', url: 'https://files.catbox.moe/wtjqmr.png' },
+  { id: 'avatar_24', url: 'https://files.catbox.moe/nez913.png' },
+  { id: 'avatar_25', url: 'https://files.catbox.moe/jzltd2.png' },
+  { id: 'avatar_26', url: 'https://files.catbox.moe/wgqytp.png' },
+  { id: 'avatar_27', url: 'https://files.catbox.moe/f9iqpj.png' },
+  { id: 'avatar_28', url: 'https://files.catbox.moe/4tv0ik.png' },
+  { id: 'avatar_29', url: 'https://files.catbox.moe/t54fk9.png' },
+  { id: 'avatar_30', url: 'https://files.catbox.moe/967h69.png' },
 ];
-
